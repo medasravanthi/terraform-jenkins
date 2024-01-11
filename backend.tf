ccad2f098e0f512a82987f809153300a0062f277
medasravanthi <150466318+medasravanthi@users.noreply.github.com>
2024-01-11 23:28:22 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "naresh-aws-test"
+    bucket = "sr-aws-test"
     key = "terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "DynamoDbLock"
